2f5514c8b3f4089c8ee4c2f37d05c426e980765e
tiochan <tiochan@gmail.com>
2022-09-26 22:43:20 +0200

    set environment vars

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,17 +1,17 @@
 export const config = {
   "dev": {
-    "username": "udagramsebas",
-    "password": "udagramsebas",
-    "database": "udagramsebas",
-    // "host": "192.168.1.141",
-    "host": "database-1.c0d8uixfjude.us-east-1.rds.amazonaws.com",
-    "dialect": "postgres",
-    "aws_region": "us-east-1",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-sgomez-dev"
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    //"host": "localhost",
+    "host": process.env.DB__HOST,
+    "dialect": process.env.DB_DIALECT,
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "jwt": {
-    "secret": " "
+    "secret": process.env.JWT_SECRET
   },
   "prod": {
     "username": "",
